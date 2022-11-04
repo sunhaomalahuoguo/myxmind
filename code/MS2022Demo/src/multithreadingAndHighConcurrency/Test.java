b1a2decede6ed32e42ba03e9a9458498aa868956
sunhaomalahuoguo <49449115+sunhaomalahuoguo@users.noreply.github.com>
2022-11-04 16:44:04 +0800

    基础知识-集合

---
+++ code/MS2022Demo/src/multithreadingAndHighConcurrency/Test.java
@@ -0,0 +1,11 @@
+package multithreadingAndHighConcurrency;
+
+import java.util.HashMap;
+
+public class Test {
+
+	public static void main(String[] args) {
+		HashMap map = new HashMap<>();
+		map.put(key, value);
+	}
+}
