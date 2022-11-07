3ec8a950f5485be6d02f126f37fad41d2e0d739b
sunhaomalahuoguo <49449115+sunhaomalahuoguo@users.noreply.github.com>
2022-11-07 16:48:43 +0800

    2

--- code/MS2022Demo/src/multithreadingAndHighConcurrency/Test.java
+++ code/MS2022Demo/src/multithreadingAndHighConcurrency/Test.java
@@ -1,11 +1,18 @@
 package multithreadingAndHighConcurrency;
 
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class Test {
 
 	public static void main(String[] args) {
+		new Object().equals(obj)
 		HashMap map = new HashMap<>();
+		HashSet set = new HashSet<>();
+		set.add(e)
+		map.put(key, value)
+		map.equals(o)
+		map.hashCode()
 		map.put(key, value);
 	}
 }
